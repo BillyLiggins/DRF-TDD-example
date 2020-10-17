3fe420b407a0349ef6d2de10b45552e2b2674f18
Billy Liggins <billy@yobota.xyz>
2020-10-17 23:58:26 +0100

    MASTER -

--- project/settings.py
+++ project/settings.py
@@ -121,22 +121,33 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = "/app/static/"
-STATIC_URL = "static/"
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/2.2/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# # STATIC_ROOT = "/app/static/"
+# STATIC_URL = "static/"
+#
+# # # Static files (CSS, JavaScript, Images)
+# # # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# #
+# # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# # STATIC_URL = '/static/'
+# # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Use nose to run all tests
 TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
