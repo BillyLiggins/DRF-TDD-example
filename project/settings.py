13d9fde16cba9aee4888499de6260f56bc8cc914
Billy Liggins <billy@yobota.xyz>
2020-10-17 20:58:47 +0100

    MASTER - d

--- project/settings.py
+++ project/settings.py
@@ -127,11 +127,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/app/static/"
 
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.9/howto/static-files/
