46e2d561c9740c7b1b37355b089b313339c35f85
Billy Liggins <billy@yobota.xyz>
2020-10-18 23:51:10 +0100

    MASTER - db env

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.environ.get("DATABASE_URL", os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
